b4a0fc2645860dedff3e23ef70cdc2df686298d3
r-ush <aarush.bhatt@gmail.com>
2021-06-01 20:14:21 +0530

    feat: post tweet

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-console.log("Hello World!!!");
+var Twit = require("twit");
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const { env } = process;
+
+var T = new Twit({
+  consumer_key: env.API_KEY,
+  consumer_secret: env.API_SECRET_KEY,
+  access_token: env.ACCESS_TOKEN,
+  access_token_secret: env.ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+  //   strictSSL: true, // optional - requires SSL certificates to be valid.
+});
+
+T.post(
+  "statuses/update",
+  { status: "Insult goes here! <3" },
+  function (err: any, data: any, response: any) {
+    console.log(data);
+  }
+);
